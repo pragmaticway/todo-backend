7b1c25e9c4041b34ebe70285d91464280723521f
Ruslan Zenin <ruslan.zenin@gmail.com>
2020-11-29 06:14:24 -0500

    added crossorigin support

--- src/main/java/com/fedex/todo/core/controller/TODOController.java
+++ src/main/java/com/fedex/todo/core/controller/TODOController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,8 +21,9 @@ import java.util.List;
  */
 @AllArgsConstructor
 @Slf4j
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/api/v1")
 public class TODOController implements TodoApi {
 
     private final TODOService service;
