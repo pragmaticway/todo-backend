01d4625fb17899d876ecc9631f29a686fede46f6
Ruslan Zenin <ruslan.zenin@gmail.com>
2020-11-29 12:10:24 -0500

    added crossorigin support for default angular port 4200

--- src/main/java/com/fedex/todo/core/controller/TODOController.java
+++ src/main/java/com/fedex/todo/core/controller/TODOController.java
@@ -21,7 +21,7 @@ import java.util.List;
  */
 @AllArgsConstructor
 @Slf4j
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1")
 public class TODOController implements TodoApi {
